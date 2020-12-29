05294a13d7b981ea0713db25c5fb78ac0b500854
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2020-12-29 15:06:58 -0800

    added StreamGobbler class to setup proper error and output streams for the import and export subprocesses

--- ExcelPlugin/src/main/java/DSSExcel.java
+++ ExcelPlugin/src/main/java/DSSExcel.java
@@ -6,6 +6,36 @@ import java.util.*;
 import java.io.*;
 
 import hec.dssgui.*;
+import hec.heclib.dss.DSSPathname;
+import hec.script.MessageBox;
+import rma.swing.text.DssPathnamePartDocument;
+
+class StreamGobbler extends Thread // from: https://www.infoworld.com/article/2071275/when-runtime-exec---won-t.html
+{
+    InputStream is;
+    String type;
+
+    StreamGobbler(InputStream is, String type)
+    {
+        this.is = is;
+        this.type = type;
+    }
+
+    public void run()
+    {
+        try
+        {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line=null;
+            while ( (line = br.readLine()) != null)
+                System.out.println(type + ">" + line);
+        } catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
+}
 
 public class DSSExcel
 {
@@ -19,10 +49,7 @@ public class DSSExcel
 
         importMenuItem.addActionListener(new ActionListener()
         {
-            public void actionPerformed(ActionEvent e)
-            {
-                plugin.importFromExcel(listSelection);
-            }
+            public void actionPerformed(ActionEvent e) { plugin.importFromExcel(listSelection); }
         });
 
         exportMenuItem.addActionListener(new ActionListener()
@@ -32,15 +59,21 @@ public class DSSExcel
                 plugin.exportToExcel(listSelection);
             }
         });
+
         listSelection.registerExportPlugin(exportMenuItem);
         listSelection.registerImportPlugin(null,importMenuItem,null);
     }
 
     private void importFromExcel(ListSelection listSelection)
     {
-
+        String oldDir = System.getProperty("user.dir");
         try
         {
+            // Set new current working directory
+            String DSSExcelDir = oldDir + "\\dotnet\\DSSExcelCLI\\";
+            System.setProperty("user.dir", DSSExcelDir);
+
+            // Get Excel file
             JFileChooser fileChooser = new JFileChooser();
             FileNameExtensionFilter filter = new FileNameExtensionFilter(
                     "Excel Files", "xls", "xlsx");
@@ -50,72 +83,119 @@ public class DSSExcel
             if (result != JFileChooser.APPROVE_OPTION)
                 return;
 
-            String executable = "C:\\Programs\\HEC-DSSVue-v3.0.1.125\\dotnet\\DSSExcel\\DSSExcelCLI.exe";
+            // Create command line argument string for DSSExcel
+            String executable = System.getProperty("user.dir") + "DSSExcelCLI.exe";
             String cOption = "import";
             String dOption = listSelection.getDSSFilename();
             String eOption = fileChooser.getSelectedFile().getAbsolutePath();
-            List<String> s = new ArrayList<>();
             String command = String.join(" ",
                     executable,
                     "-c " + cOption,
                     "-d " + dOption,
                     "-e " + eOption);
+
             Runtime run  = Runtime.getRuntime();
             System.out.println(command);
             Process proc = run.exec(command);
+
+            // setting up proper error stream for import process
+            StreamGobbler errorGobbler = new
+                    StreamGobbler(proc.getErrorStream(), "ERROR");
+
+            // setting up proper output stream for import process
+            StreamGobbler outputGobbler = new
+                    StreamGobbler(proc.getInputStream(), "OUTPUT");
+
+            // start streams
+            errorGobbler.start();
+            outputGobbler.start();
+
             proc.waitFor();
-            System.out.println(proc.exitValue());
+
+            // change current directory to old directory
+            System.setProperty("user.dir", oldDir);
+
+            // refresh DSS file in DSSVue
             listSelection.closeDssFile();
             listSelection.openDSSFile(dOption);
+
+            // Display Import Status
+            if (proc.exitValue() == 0)
+                MessageBox.showInformation("Import succeeded.", "Import Status");
+            else
+                MessageBox.showError("Import failed.", "Import Status");
         }
         catch (Exception e)
         {
+            System.setProperty("user.dir", oldDir);
             e.printStackTrace();
         }
     }
 
     private void exportToExcel(ListSelection listSelection)
     {
+        String oldDir = System.getProperty("user.dir");
         try
         {
+            // Check if records have been selected in DSSVue
+            if (listSelection.getNumberSelectedPathnames() == 0)
+            {
+                MessageBox.showError("Select DSS records to export.", "Export Error");
+                return;
+            }
+
+            // Set new current working directory
+            String DSSExcelDir = oldDir + "\\dotnet\\DSSExcelCLI\\";
+            System.setProperty("user.dir", DSSExcelDir);
+
+            // Get Excel file
             JFileChooser fileChooser = new JFileChooser();
             FileNameExtensionFilter filter = new FileNameExtensionFilter(
                     "Excel Files", "xls", "xlsx");
             fileChooser.setAcceptAllFileFilterUsed(false);
             fileChooser.setFileFilter(filter);
-            int result = fileChooser.showSaveDialog(null);
+            int result = fileChooser.showOpenDialog(null);
             if (result != JFileChooser.APPROVE_OPTION)
                 return;
 
+            // Create command line argument string for DSSExcel
             DataReferenceSet v = listSelection.getSelectedPathnames();
-            String executable = "C:\\Programs\\HEC-DSSVue-v3.0.1.125\\dotnet\\DSSExcel\\DSSExcelCLI.exe";
+            String executable = System.getProperty("user.dir") + "DSSExcelCLI.exe";
             String cOption = "export";
             String dOption = listSelection.getDSSFilename();
             String eOption = fileChooser.getSelectedFile().getAbsolutePath();
-            List<String> s = new ArrayList<>();
-            for (int i = 0; i < v.size(); i++)
-                s.add("Export" + (i + 1));
-            String sOptions = String.join(",", s);
             List<String> p = new ArrayList<>();
-            for (int i = 0; i < v.size(); i++)
-                p.add(v.elementAt(i).pathname());
-            String pOptions = String.join(",", p);
+            for (int i = 0; i < v.size(); i++) // Get selected paths
+            {
+                DSSPathname path = new DSSPathname();
+                path.setPathname(v.elementAt(i).pathname());
+                p.add("/" + path.aPart() + "/" + path.bPart() + "/" + path.cPart() + "//" + path.ePart() + "/" + path.fPart() + "/");
+            }
+            String pOptions = String.join(";", p);
             pOptions = "\"" + pOptions + "\"";
             String command = String.join(" ",
                     executable,
                     "-c " + cOption,
                     "-d " + dOption,
                     "-e " + eOption,
-                    "-s " + sOptions,
                     "-p " + pOptions);
             Runtime run  = Runtime.getRuntime();
             System.out.println(command);
             Process proc = run.exec(command);
             proc.waitFor();
-            System.out.println(proc.exitValue());
+
+            // change current directory to old directory
+            System.setProperty("user.dir", oldDir);
+
+            // Display Export Status
+            if (proc.exitValue() == 0)
+                MessageBox.showInformation("Export succeeded.", "Export Status");
+            else
+                MessageBox.showError("Export failed.", "Export Status");
         }
         catch (Exception e)
         {
+            System.setProperty("user.dir", oldDir);
             e.printStackTrace();
         }
     }
